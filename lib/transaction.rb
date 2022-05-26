447cf1cd5494ce215b3651a11b7b076e7a8b752e
Patryk Okpara <patrykokpara@hotmail.com>
2022-05-26 23:52:05 +0100

    Transaction class withdrawl method  sets transaction type to 'debit'

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,20 +1,16 @@
 class Transaction
-
+    attr_reader :type, :amount
     def initialize
         @type
-        @amount
-    end
-
-    def type
-        @type
-    end
-
-    def amount
         @amount
     end
 
     def deposit(amount)
         @type = 'credit'
         @amount = amount
+    end
+
+    def withdrawl(amount)
+        @type = 'debit'
     end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -13,4 +13,11 @@ describe Transaction do
             expect(subject.amount).to eq 10
         end
     end
+
+    describe '#withdrawl' do 
+        it 'sets type to debit' do
+            subject.withdrawl(10)
+            expect(subject.type).to eq 'debit'
+        end
+    end
 end
