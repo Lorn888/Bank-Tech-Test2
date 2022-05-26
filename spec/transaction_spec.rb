8d5b234d5cdc31e29f8b780339086fa81057b08f
Patryk Okpara <patrykokpara@hotmail.com>
2022-05-26 23:34:59 +0100

    Transaction class deposit method  sets amount to the amount passed

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,25 +2,15 @@ require './lib/transaction'
 
 describe Transaction do
 
-
-    subject(:transaction) { described_class.new('credit', 100) }
-    
-    describe '#type' do
-        it 'returns type instance variable' do
-            expect(subject.type).to eq 'credit'
-        end
-    end
-
-    describe '#amount' do
-        it 'returns amount instance variable' do
-            expect(subject.amount).to eq 100
-        end
-    end
-
     describe '#deposit' do
         it 'sets type to credit' do
             subject.deposit(10)
             expect(subject.type).to eq 'credit'
+        end
+
+        it 'sets amount to the amount passed' do
+            subject.deposit(10)
+            expect(subject.amount).to eq 10
         end
     end
 end
