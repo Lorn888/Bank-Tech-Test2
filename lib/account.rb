48f5d8b82ee4be220975e39e10deffbaa657625b
Patryk Okpara <patrykokpara@hotmail.com>
2022-05-27 23:33:31 +0100

    Refactored increasing and decrasing balance methods

--- lib/account.rb
+++ lib/account.rb
@@ -8,16 +8,16 @@ class Account
     end
 
     def deposit(amount, transaction = new_transaction)
-        @balance += amount
         transaction.deposit(amount)
         @transactions << transaction
+        increase_balance(amount)
         "Succesful deposit of #{amount}"
     end
 
     def withdrawl(amount, transaction = new_transaction)
-        @balance -= amount
         transaction.withdrawl(amount)
         @transactions << transaction
+        decrease_balance(amount)
         "Succesful withdrawl of #{amount}"
     end
     
@@ -29,5 +29,13 @@ class Account
 
     def new_transaction
         Transaction.new
+    end
+
+    def increase_balance(amount)
+        @balance += amount
+    end
+
+    def decrease_balance(amount)
+        @balance -= amount
     end
 end
